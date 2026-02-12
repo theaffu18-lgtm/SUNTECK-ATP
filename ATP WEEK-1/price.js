f6183c4d512d3bcb52cc902a42a634bbe2be6011
MOHAMMAD AFTHAB <theaffu18@gmail.com>
2026-02-12 21:10:47 +0530

    Delete ATP WEEK-1 directory

--- ATP WEEK-1/price.js
+++
@@ -1,7 +0,0 @@
-let price =1299
-if (price<500)
-    console.log("budget course")
-else if (price>=500 && price<1000)
-    console.log("standard course")
-else
-    console.log("expensive course")
--- ATP WEEK-1/week1.2.js
+++
@@ -1,6 +0,0 @@
-let ta=0
-ta+=500
-ta+=1200
-ta-=200
-ta+=ta*0.18
-console.log(ta)
